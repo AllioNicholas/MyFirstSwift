cf7400a5a21572368ef3babe0c4e2ed583e825ac
Nicholas Allio <Nick@MacBook-Pro-di-Nicholas.local>
2015-09-21 22:48:01 +0200

    lesson 2 completed

--- MyFirstSwift/ViewController.swift
+++ MyFirstSwift/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var recLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        recLabel.hidden = false
+    }
 
+    @IBAction func stopRecord(sender: UIButton) {
+        recLabel.hidden = true
+    }
 }
 

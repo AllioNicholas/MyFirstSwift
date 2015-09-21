0a23445af2b7f49b6daf6312f839733b0f7aece1
Nicholas Allio <Nick@MacBook-Pro-di-Nicholas.local>
2015-09-21 21:11:21 +0200

    initial commit with uploading of project

---
+++ MyFirstSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstSwift
+//
+//  Created by Nicholas Allio on 21/09/15.
+//  Copyright © 2015 Nicholas Allio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
